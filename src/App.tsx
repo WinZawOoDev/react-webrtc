251410a95e931e95bd1220dfc042fde72b3a1c1b
WinZawOoDev <winzawoo.dev@gmail.com>
2023-11-30 22:48:07 +0630

    add copy clipboard func

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,31 @@ import { Input } from './components/ui/input'
 import { FaRegCopy } from "react-icons/fa"
 import usePeer from './hooks/usePeer'
 import { Skeleton } from './components/ui/skeleton'
+import { useEffect, useRef } from 'react'
+import { useCopyToClipboard } from 'usehooks-ts'
+import { useToast } from './components/ui/use-toast'
+import { Toaster } from './components/ui/toaster'
+import { cn } from './lib/utils'
+
 
 function App() {
 
   const { peer, starting } = usePeer();
+  const peerIDRef = useRef<HTMLSpanElement>(null);
+  const [value, copy] = useCopyToClipboard();
+  const { toast } = useToast();
+
+
+  useEffect(() => {
+    if (value) {
+      toast({
+        variant: 'default',
+        description: `${value} copied`
+      })
+    }
+
+  }, [value])
+
 
   return (
     <main className='relative container flex-1 h-screen w-screen p-10 bg-gray-50'>
@@ -25,15 +46,20 @@ function App() {
         </div>
         <div className='flex my-3 items-center justify-between'>
           {!starting ?
-            <span className='mx-1 text-center w-full text-sm font-bold tracking-wide'>{peer?.id}</span>
+            <span ref={peerIDRef} className='mx-1 text-center w-full text-sm font-bold tracking-wide'>{peer?.id}</span>
             : <Skeleton className='h-9 w-full mx-1' />}
-          <Button disabled={starting} className='px-6 hover:bg-gray-800'>
+          <Button
+            onClick={() => copy(peerIDRef.current?.innerText!)}
+            disabled={starting}
+            className='px-6 hover:bg-gray-800'
+          >
             <span className='text-xl hover:text-gray-500'>
               <FaRegCopy />
             </span>
           </Button>
         </div>
       </Card>
+      <Toaster />
     </main>
   )
 }
